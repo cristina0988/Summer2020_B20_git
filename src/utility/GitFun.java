55a15b02ac11d3ac6c7ce5b197a77396fdfee209
Cristina <cristinaleca9@gmail.com>
2020-08-05 20:54:54 -0400

    add GitFun.java

---
+++ src/utility/GitFun.java
@@ -0,0 +1,11 @@
+package utility;
+
+public class GitFun {
+
+    // here we create a new code or import a code from previous classes
+
+    public static void main(String[] args) {
+
+        System.out.println("Everything is fun");
+    }
+}
