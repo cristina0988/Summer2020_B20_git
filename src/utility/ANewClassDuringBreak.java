2d52acfa07fe79a5e9ac68b2e0b04c8d1d54070b
Cristina <cristinaleca9@gmail.com>
2020-08-05 21:04:53 -0400

    add ANewClassDuringBreak.java

---
+++ src/utility/ANewClassDuringBreak.java
@@ -0,0 +1,10 @@
+package utility;
+
+public class ANewClassDuringBreak {
+
+    //here we write new code or importing code from somewhere. In my case I will create new code
+    public static void main(String[] args) {
+        System.out.println("Here I am going to write smth very beautifully");
+    }
+
+}
